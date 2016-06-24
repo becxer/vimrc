ea69075fc91d1713a9d03657027d76363d55abf9
becxer <becxer87@gmail.com>
2016-06-25 00:16:49 +0900

    remove jedivim

--- install-vim.sh
+++ install-vim.sh
@@ -2,12 +2,6 @@
 sudo apt-get install curl
 sudo apt-get install exuberant-ctags
 
-#install library for default
-sudo apt-get install python-pip
-
-#install through pip
-sudo pip install jedi
-
 #replace vimrc
 cp ~/.vimrc ~/.vimrc.bk
 cp ./data/vimrc ~/.vimrc
@@ -23,15 +17,7 @@ cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/aperezdc/vim-template.git
-git clone --recursive https://github.com/davidhalter/jedi-vim.git
 git clone https://github.com/google/vim-searchindex.git
-
-#install for vifm (newer version & past version is not working..)
-tar -jxvf ./data/vifm-0.7.7.tar.bz2
-cd vifm-0.7.7
-sudo ./configure
-sudo make
-sudo make install
 
 echo "..."
 echo "=================================================="
